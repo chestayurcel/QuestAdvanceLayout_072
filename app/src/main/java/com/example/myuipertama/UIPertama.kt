56aba9f27c6452ea184c1ca9f6d6eae59bf3302e
Yurcell <chestayurcel@gmail.com>
2025-10-23 12:19:54 +0700

    Menambahkan padding bawah pada Teks copyright

--- app/src/main/java/com/example/myuipertama/UIPertama.kt
+++ app/src/main/java/com/example/myuipertama/UIPertama.kt
@@ -76,6 +76,7 @@ fun ActivitasPertama(modifier: Modifier){
                 stringResource(R.string.copy),
                 modifier = Modifier
                     .align(Alignment.BottomCenter)
+                    .padding(bottom = 50.dp)
         }
     }
 }
