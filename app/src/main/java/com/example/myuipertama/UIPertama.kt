c45b0aaf3d885487c754a59a9fca9dd703bfd18c
Yurcell <chestayurcel@gmail.com>
2025-10-23 11:41:10 +0700

    Menambahkan Text, Spacer, dan Card di Halaman Utama

--- app/src/main/java/com/example/myuipertama/UIPertama.kt
+++ app/src/main/java/com/example/myuipertama/UIPertama.kt
@@ -1,7 +1,14 @@
 package com.example.myuipertama
 
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import java.lang.reflect.Modifier
 
 @Composable
@@ -10,6 +17,15 @@ fun ActivitasPertama(modifier: Modifier){
         .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
         ) {
-        Text()
+        Text(stringResource(R.string.prodi),
+            fontSize = 35.sp,
+            fontWeight = FontWeight.Bold)
+        Text(stringResource(R.string.univ),
+            fontSize = 22.sp)
+        Spacer(modifier = Modifier.height(25.dp))
+        Card (modifier = Modifier
+            .fillMaxWidth(1f)
+            .padding(12.dp),
+            )
     }
 }
