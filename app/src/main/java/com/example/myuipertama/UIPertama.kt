228265cf5c804f44d8a69a4da73cd355a507e7b5
Yurcell <chestayurcel@gmail.com>
2025-10-23 11:25:14 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myuipertama/UIPertama.kt
@@ -0,0 +1,15 @@
+package com.example.myuipertama
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import java.lang.reflect.Modifier
+
+@Composable
+fun ActivitasPertama(modifier: Modifier){
+    Column(modifier = Modifier.padding(top=100.dp)
+        .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+        Text()
+    }
+}
